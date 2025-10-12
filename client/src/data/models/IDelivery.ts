f35132fed988eb8f3f3d28d90197db2c2e41c596
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-10-12 13:44:28 -0300

    feat(client): creating IDelivery interface

---
+++ client/src/data/models/IDelivery.ts
@@ -0,0 +1,40 @@
+import { IShoppingCartItem } from "./ITable";
+
+export enum DeliveryStatus {
+    PENDING = "Pending",
+    IN_PROGRESS = "In Progress",
+    DELIVERED = "Delivered",
+    CANCELED = "Canceled",
+}
+export const DeliveryStatusLabels: { [key in DeliveryStatus]: string } = {
+    [DeliveryStatus.PENDING]: "Pendente",
+    [DeliveryStatus.IN_PROGRESS]: "Em Progresso",
+    [DeliveryStatus.DELIVERED]: "Entregue",
+    [DeliveryStatus.CANCELED]: "Cancelado",
+}
+
+export enum PaymentMethod {
+    CASH = "Cash",
+    CREDIT_CARD = "Credit Card",
+    DEBIT_CARD = "Debit Card",
+    PIX = "Pix",
+}
+
+export const PaymentMethodLabels: { [key in PaymentMethod]: string } = {
+    [PaymentMethod.CASH]: "Dinheiro",
+    [PaymentMethod.CREDIT_CARD]: "Cartão de Crédito",
+    [PaymentMethod.DEBIT_CARD]: "Cartão de Débito",
+    [PaymentMethod.PIX]: "Pix",
+}
+
+export interface IDelivery {
+    id: string;
+    customerName: string;
+    customerPhone: string;
+    address: string;
+    status: DeliveryStatus;
+    paymentMethod: PaymentMethod;
+    changeOfMoney?: number;
+    items: IShoppingCartItem[]
+    totalAmount: number;
+}
