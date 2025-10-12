0ef97a8b681a5ff04ed51b0f6d975ffdb392cf0d
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-10-12 13:45:11 -0300

    feat(client): adding product category labels

--- client/src/data/models/IProduct.ts
+++ client/src/data/models/IProduct.ts
@@ -7,6 +7,15 @@ export enum ProductCategory {
     OTHERS = "Others",
 }
 
+export const ProductCategoryLabels: { [key in ProductCategory]: string } = {
+    [ProductCategory.COLDDRINKS]: "Bebidas Frias",
+    [ProductCategory.HOTDRINKS]: "Bebidas Quentes",
+    [ProductCategory.PORTIONS]: "Porções",
+    [ProductCategory.BARBECUE]: "Churrasco",
+    [ProductCategory.LUNCHES]: "Almoços",
+    [ProductCategory.OTHERS]: "Outros",
+}
+
 export interface IProduct {
     id: string;
     name: string;
