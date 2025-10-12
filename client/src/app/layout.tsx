68a237a2ec1ad119028566654f67dff22b763dde
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-10-12 08:19:40 -0300

    fix(client): setting cz-shortcut-listen as false

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -85,6 +85,7 @@ export default function RootLayout({
     <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        cz-shortcut-listen="false"
       >
         {children}
       </body>
