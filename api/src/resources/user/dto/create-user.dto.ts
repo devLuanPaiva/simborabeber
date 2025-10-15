acff52cf76e47a212a536d0b23d80724b2f5d134
Luan Alves <devluanpaiva@gmail.com>
2025-10-15 07:55:39 -0300

    feat(api): creating user dto

--- api/src/resources/user/dto/create-user.dto.ts
+++ api/src/resources/user/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { ApiProperty } from "@nestjs/swagger"
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, MinLength } from "class-validator"
+import { Role } from "../entities/user.entity"
+export class CreateUserDto {
+    @IsNotEmpty({ message: "O nome é obrigatório" })
+    @ApiProperty({ example: "João Silva" })
+    @IsString({ message: "O nome deve ser uma string" })
+    name: string
+
+    @IsEmail({}, { message: "E-mail inválido" })
+    @ApiProperty({ example: "joao@email.com" })
+    email: string
+
+    @IsOptional()
+    @MinLength(6, { message: "A senha deve ter no mínimo 6 caracteres" })
+    @ApiProperty({ example: "senha123", required: false })
+    password?: string
+
+    @IsEnum(Role)
+    @ApiProperty({ example: "WAITER" })
+    @IsString({ message: "O papel deve ser uma string" })
+    role?: Role
+
+
+}
--- api/src/resources/user/dto/update-user.dto.ts
+++ api/src/resources/user/dto/update-user.dto.ts
@@ -1,4 +1,11 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+
+    @IsOptional()
+    @IsBoolean({ message: "isActive deve ser true ou false" })
+    @ApiProperty({ example: true, required: false })
+    isActive?: boolean
+}
