c93e0e6dbb031cca9c71ace00643fe3ad086b291
Luan Alves <devluanpaiva@gmail.com>
2025-10-15 09:32:26 -0300

    feat(api): implementing user service

--- api/src/resources/user/user.service.ts
+++ api/src/resources/user/user.service.ts
@@ -1,26 +1,131 @@
-import { Injectable } from '@nestjs/common';
+import { PrismaService } from './../../database/prisma.service';
+import { Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { handleException } from 'src/functions/handleException';
+import { Role } from './entities/user.entity';
+import * as bcrypt from "bcrypt"
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prismaService: PrismaService) { }
+  async createUser(data: CreateUserDto) {
+    try {
+
+      await this.ensureEmailNotExists(data.email);
+
+      const passwordHash = await bcrypt.hash(data.password, 10);
+      const response = await this.prismaService.user.create({
+        data: {
+          name: data.name,
+          email: data.email,
+          password: passwordHash,
+          role: data.role ?? Role.WAITER,
+
+        },
+      });
+      return response;
+    } catch (error) {
+      handleException(error, "Erro ao criar usuário");
+    }
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async getAllUsers() {
+    try {
+
+      const users = await this.prismaService.user.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          isActive: true,
+          createdAt: true
+        }
+      });
+      return users;
+    } catch (error) {
+      handleException(error, "Erro ao buscar usuários");
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async getUserById(id: string,) {
+    try {
+      const response = await this.prismaService.user.findUnique({
+        where: { id },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          isActive: true,
+          createdAt: true
+        }
+      });
+      if (!response) {
+        throw new NotFoundException({ error: 'O usuário não foi encontrado', message: "Usuário não encontrado", code: "USER_NOT_FOUND" });
+      }
+      return response;
+    } catch (error) {
+      handleException(error, "Erro ao buscar usuário");
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async updateUser(id: string, data: UpdateUserDto) {
+    try {
+      const response = await this.prismaService.user.update({
+        where: { id },
+        data: {
+          name: data.name,
+          isActive: data.isActive
+        },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          isActive: true,
+          createdAt: true
+        }
+      });
+      return response;
+    } catch (error) {
+      handleException(error, "Erro ao atualizar usuário");
+    }
+  }
+  async deleteUser(id: string) {
+    try {
+      const response = await this.prismaService.user.delete({
+        where: { id },
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          role: true,
+          isActive: true,
+          createdAt: true
+        }
+      });
+      return response;
+    } catch (error) {
+      handleException(error, "Erro ao deletar usuário");
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  private async ensureEmailNotExists(email: string) {
+    const emailExists = await this.prismaService.user.findUnique({
+      where: { email },
+    });
+    if (emailExists) {
+      throw new UnauthorizedException(
+        {
+          error: "E-mail já cadastrado.",
+          message: "E-mail já cadastrado.",
+          code: "EMAIL_EXISTS",
+          field: "email"
+        }
+      );
+    }
   }
+
 }
