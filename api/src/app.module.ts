23903e5cb6c1f008902030d92bfbad89e7a206bc
Luan Alves <devluanpaiva@gmail.com>
2025-10-15 08:10:54 -0300

    feat(api): crating prisma service module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './resources/user/user.module';
+import { PrismaModule } from './database/prisma.module';
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
