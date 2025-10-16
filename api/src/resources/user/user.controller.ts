a6ed1cb4ce9fa7dd13bcfb183a6b0ce20aaad5d2
Luan Alves <devluanpaiva@gmail.com>
2025-10-16 07:23:16 -0300

    feat(api): creating user controller

--- api/src/resources/user/user.controller.ts
+++ api/src/resources/user/user.controller.ts
@@ -2,33 +2,52 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+
 
 @Controller('user')
+@ApiTags('User')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @ApiOperation({ summary: "Criar um novo usuário" })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, description: "Usuário criado com sucesso" })
+  createUser(@Body() createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
   }
 
   @Get()
-  findAll() {
-    return this.userService.findAll();
+  @ApiOperation({ summary: "Listar todos os usuários" })
+  @ApiResponse({ status: 200, description: "Lista retornada com sucesso" })
+  getAllUsers() {
+    return this.userService.getAllUsers();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+  @ApiOperation({ summary: "Buscar um usuário por ID" })
+  @ApiParam({ name: "id", type: String })
+  @ApiResponse({ status: 200, description: "Usuário encontrado" })
+  @ApiResponse({ status: 404, description: "Usuário não encontrado" })
+  getUserById(@Param('id') id: string) {
+    return this.userService.getUserById(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: "Atualizar um usuário" })
+  @ApiParam({ name: "id", type: String })
+  @ApiBody({ type: UpdateUserDto })
+  @ApiResponse({ status: 200, description: "Usuário atualizado com sucesso" })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    return this.userService.updateUser(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: "Deletar um usuário" })
+  @ApiParam({ name: "id", type: String })
+  @ApiResponse({ status: 200, description: "Usuário deletado com sucesso" })
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.deleteUser(id);
   }
 }
