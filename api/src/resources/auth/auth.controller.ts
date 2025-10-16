38112524ed235a4825c4b533160b6f2eb1295d5b
Luan Alves <devluanpaiva@gmail.com>
2025-10-16 07:56:34 -0300

    feat(api): implementing auth controller

--- api/src/resources/auth/auth.controller.ts
+++ api/src/resources/auth/auth.controller.ts
@@ -1,34 +1,19 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthDto } from './dto/auth.dto';
+
 
 @Controller('auth')
+@ApiTags('Auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
-
-  @Post()
-  create(@Body() createAuthDto: CreateAuthDto) {
-    return this.authService.create(createAuthDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.authService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.authService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateAuthDto: UpdateAuthDto) {
-    return this.authService.update(+id, updateAuthDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.authService.remove(+id);
+  constructor(private readonly authService: AuthService) { }
+  @Post("login")
+  @ApiOperation({ summary: "Login do usuário" })
+  @ApiBody({ type: AuthDto })
+  @ApiResponse({ status: 200, description: "Login realizado com sucesso" })
+  @ApiResponse({ status: 401, description: "Credenciais inválidas" })
+  async loginOrganizer(@Body() { email, password }: AuthDto) {
+    return this.authService.signIn({ email, password })
   }
 }
