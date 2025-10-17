5ca4d920c83ebd06ebc9ca12319eeab7b6106eb6
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-10-17 19:23:54 -0300

    chore(api): configuring the JWT module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -4,9 +4,13 @@ import { AppService } from './app.service';
 import { UserModule } from './resources/user/user.module';
 import { PrismaModule } from './database/prisma.module';
 import { AuthModule } from './resources/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 @Module({
-  imports: [UserModule, PrismaModule, AuthModule],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+  }), UserModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule { }
+  
--- api/src/resources/auth/auth.module.ts
+++ api/src/resources/auth/auth.module.ts
@@ -1,9 +1,34 @@
+import { JwtModule } from "@nestjs/jwt"
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { ConfigModule, ConfigService } from "@nestjs/config"
 
 @Module({
-  controllers: [AuthController],
+  imports: [
+    ConfigModule,
+    JwtModule.registerAsync({
+      global: true,
+      inject: [ConfigService],
+      imports: [ConfigModule],
+      useFactory: (config: ConfigService) => {
+        const expiration = config.get<string>("JWT_EXPIRATION") ?? "2h";
+
+        const expiresInSeconds = expiration.endsWith("h")
+          ? Number(expiration.slice(0, -1)) * 3600
+          : Number(expiration);
+
+        return {
+          secret: config.get<string>("JWT_SECRET"),
+          signOptions: {
+            expiresIn: expiresInSeconds,
+          },
+        };
+      }
+
+    }),
+  ],
   providers: [AuthService],
+  controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule { }
