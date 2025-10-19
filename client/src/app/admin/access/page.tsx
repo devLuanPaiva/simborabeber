f68aa147bf4032cbffc01680a550edf44f38a595
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-10-19 14:56:03 -0300

    feat(client): implementing logic after login

--- client/src/app/admin/access/page.tsx
+++ client/src/app/admin/access/page.tsx
@@ -4,18 +4,59 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/com
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { useAuth } from "@/data/contexts";
+import { decodeTokenToUser } from "@/data/functions";
+import { UserRole } from "@/data/models";
 import { Eye, EyeOff, Flame, Lock, LogIn, Mail } from "lucide-react";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 export default function AccessPage() {
     const { login } = useAuth()
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [showPassword, setShowPassword] = useState<boolean>(false);
+    const [errorMessage, setErrorMessage] = useState<string | null>(null);
+    const [isLoadingOperation, setIsLoadingOperation] = useState<boolean>(false);
 
+    const navigate = useRouter();
     async function handleSubmit(event: React.FormEvent) {
         event.preventDefault();
-        await login(email, password);
+        setIsLoadingOperation(true);
+        setErrorMessage(null);
+
+        try {
+            const response = await login(email, password);
+
+            if (response.error) {
+                setErrorMessage(response.error.detail);
+                return;
+            }
+            const loggedInUser = decodeTokenToUser(response?.data[0]?.access_token);
+
+            checkRoleAccessAndRedirect(loggedInUser?.role!);
+        } catch (error) {
+            setErrorMessage("Ocorreu um erro ao tentar efetuar o login. Por favor, tente novamente.");
+
+            console.error("Login error:", error);
+        } finally {
+            setIsLoadingOperation(false);
+        }
     }
+
+    const checkRoleAccessAndRedirect = (role: UserRole) => {
+        const roleRoutes: Record<UserRole, string> = {
+            [UserRole.ADMIN]: "/admin",
+            [UserRole.MANAGER]: "/admin/dashboard",
+            [UserRole.WAITER]: "/admin/area-garcom",
+        };
+
+        const route = roleRoutes[role];
+
+        if (route) {
+            navigate.push(route, { scroll: false });
+        } else {
+            setErrorMessage("Seu papel de usuário não tem acesso a esta área.");
+        }
+    };
     return (
         <div className="min-h-screen bg-gradient-to-br from-background via-background to-primary/5 flex items-center justify-center p-4">
             <Card className="w-full max-w-md">
@@ -66,10 +107,24 @@ export default function AccessPage() {
                         </div>
 
                         <Button type="submit" className="w-full gap-2 cursor-pointer" size="lg">
-                            <LogIn className="h-5 w-5" />
-                            Entrar
+                            {isLoadingOperation ? (
+                                <>
+                                    <div className="w-5 h-5 border-2 border-white border-t-transparent rounded-full animate-spin mr-2 "></div>
+                                    Processando...
+                                </>
+                            ) : (
+                                <>
+                                    <LogIn className="h-5 w-5" />
+                                    Entrar
+                                </>
+                            )}
                         </Button>
                     </form>
+                    {errorMessage && (
+                        <div className="mt-4 bg-red-300/20 p-3 rounded border border-red-400">
+                            <p className=" text-sm text-red-600 text-center">{errorMessage}</p>
+                        </div>
+                    )}
                 </CardContent>
             </Card>
         </div>
