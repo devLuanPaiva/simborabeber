19fa5f4aa636ec81e17b255a6f4ad25ca0dc64a1
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-10-26 14:04:47 -0300

    feat(api): implementing establishment dto

--- api/src/resources/establishment/dto/update-establishment.dto.ts
+++ api/src/resources/establishment/dto/update-establishment.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
 import { CreateEstablishmentDto } from './create-establishment.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateEstablishmentDto extends PartialType(CreateEstablishmentDto) {}
+export class UpdateEstablishmentDto extends PartialType(CreateEstablishmentDto) {
+  @IsOptional()
+  @IsBoolean({ message: 'isActive deve ser true ou false' })
+  @ApiProperty({ example: true, required: false })
+  isActive?: boolean;
+}
