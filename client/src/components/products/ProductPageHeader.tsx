e5e009d6bf77672a208dcc7a963d27390a947f6a
Luan Alves <97049835+Luan029@users.noreply.github.com>
2025-10-29 19:29:12 -0300

    feat(client): creating ProductPageHeader component

---
+++ client/src/components/products/ProductPageHeader.tsx
@@ -0,0 +1,21 @@
+import { Plus } from "lucide-react";
+import { Button } from "../ui/button";
+
+interface ProductPageHeaderProps {
+    handleOpenDialog: () => void;
+}
+export function ProductPageHeader({ handleOpenDialog }: Readonly<ProductPageHeaderProps>) {
+    return <header className="flex items-center justify-between mb-6">
+        <div>
+            <h1 className="text-3xl mb-2">Produtos</h1>
+            <p className="text-muted-foreground">
+                Gerenciamento do cardápio e produtos
+            </p>
+        </div>
+        <Button onClick={() => handleOpenDialog()} className="gap-2">
+            <Plus className="h-5 w-5 cursor-pointer" />
+            Novo Produto
+        </Button>
+
+    </header>
+}
