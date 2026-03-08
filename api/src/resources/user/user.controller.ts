d6b0596d6f52921f54d488ec3debd35a058831b7
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-03-08 09:57:33 -0300

    feat(api): implementing user controller

--- api/src/resources/user/user.controller.ts
+++ api/src/resources/user/user.controller.ts
@@ -1,34 +1,60 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from "@nestjs/swagger"
+
 
 @Controller('user')
+@ApiTags('users')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(private readonly userService: UserService) { }
 
   @Post()
+  @ApiOperation({ summary: "Criar um novo usuário" })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, description: "Usuário criado com sucesso" })
+  @ApiResponse({ status: 400, description: "Requisição inválida" })
+  @HttpCode(HttpStatus.CREATED)
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
   @Get()
+  @ApiOperation({ summary: "Listar todos os usuários" })
+  @ApiResponse({ status: 200, description: "Usuários listados com sucesso" })
+  @ApiResponse({ status: 400, description: "Requisição inválida" })
+  @HttpCode(HttpStatus.OK)
   findAll() {
     return this.userService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: "Obter um usuário pelo ID" })
+  @ApiResponse({ status: 200, description: "Usuário encontrado com sucesso" })
+  @ApiResponse({ status: 404, description: "Usuário não encontrado" })
+  @HttpCode(HttpStatus.OK)
   findOne(@Param('id') id: string) {
-    return this.userService.findOne(+id);
+    return this.userService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: "Atualizar um usuário pelo ID" })
+  @ApiBody({ type: UpdateUserDto })
+  @ApiResponse({ status: 200, description: "Usuário atualizado com sucesso" })
+  @ApiResponse({ status: 400, description: "Requisição inválida" })
+  @ApiResponse({ status: 404, description: "Usuário não encontrado" })
+  @HttpCode(HttpStatus.OK)
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.userService.update(+id, updateUserDto);
+    return this.userService.update(id, updateUserDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: "Remover um usuário pelo ID" })
+  @ApiResponse({ status: 200, description: "Usuário removido com sucesso" })
+  @ApiResponse({ status: 404, description: "Usuário não encontrado" })
+  @HttpCode(HttpStatus.OK)
   remove(@Param('id') id: string) {
-    return this.userService.remove(+id);
+    return this.userService.remove(id);
   }
 }
