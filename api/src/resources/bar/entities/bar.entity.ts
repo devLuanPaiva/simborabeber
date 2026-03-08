1255d803e8fae89629c1f510a7687591f88b5c5c
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-03-08 16:08:45 -0300

    feat(api): Creating a table of bars and associating them with users.

--- api/src/resources/bar/entities/bar.entity.ts
+++ api/src/resources/bar/entities/bar.entity.ts
@@ -1,1 +1,51 @@
-export class Bar {}
+import { UserEntity } from "../../user/entities/user.entity";
+import { Column, CreateDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export enum AccessPlan {
+    BASIC = 'basic',
+    MEDIUM = 'medium',
+    PREMIUM = 'premium',
+}
+
+@Entity({ name: 'bars', schema: 'public' })
+export class BarEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        type: 'varchar',
+        length: 120
+    })
+    name: string;
+
+    @Column({
+        type: 'varchar',
+        length: 120,
+        unique: true
+    })
+    slug: string;
+
+    @Column({
+        type: 'varchar',
+        length: 255,
+        nullable: true
+    })
+    image: string;
+
+    @Column({
+        type: 'enum',
+        enum: AccessPlan,
+        default: AccessPlan.BASIC,
+        name: 'access_plan'
+    })
+    accessPlan: AccessPlan;
+
+    @OneToMany(() => UserEntity, user => user.bar)
+    users: UserEntity[];
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date;
+}
