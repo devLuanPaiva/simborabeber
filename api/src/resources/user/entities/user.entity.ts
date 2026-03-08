eb3ab359021bb723a2b266f399fad2be4901e7fc
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-03-08 09:53:30 -0300

    feat(api): implementing user entity and user dto

--- api/src/resources/user/entities/user.entity.ts
+++ api/src/resources/user/entities/user.entity.ts
@@ -1,1 +1,45 @@
-export class User {}
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn,
+    
+} from 'typeorm';
+
+export enum UserRole {
+    ADMIN = 'admin',
+    MANAGER = 'manager',
+    WAITER = 'waiter',
+}
+
+@Entity({ name: 'users', schema: 'public' })
+export class UserEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column({
+        type: 'varchar',
+        length: 120
+    })
+    name: string;
+
+    @Column({ unique: true })
+    email: string;
+
+    @Column({
+        type: 'enum',
+        enum: UserRole,
+        default: UserRole.WAITER
+    })
+    role: UserRole;
+
+    @Column()
+    password: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updatedAt: Date;
+}
