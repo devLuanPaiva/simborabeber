1e7fb3349f396f63833b08f12137852b88b0d285
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-03-08 14:05:15 -0300

    feat(api): creating auth resource

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { UserModule } from './resources/user/user.module';
+import { AuthModule } from './resources/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule, UserModule],
+  imports: [DatabaseModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
