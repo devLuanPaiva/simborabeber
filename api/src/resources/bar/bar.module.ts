7ba281a1c0f78872505e8a0eec2272068b72709f
Luan Alves <devluanpaiva@gmail.com>
2026-03-09 13:05:08 -0300

    feat(api): exporting bar repository and service

--- api/src/resources/bar/bar.module.ts
+++ api/src/resources/bar/bar.module.ts
@@ -3,10 +3,12 @@ import { BarService } from './bar.service';
 import { BarController } from './bar.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { BarEntity } from './entities/bar.entity';
+import { BarRepository } from './repository/bar.repository';
 
 @Module({
   controllers: [BarController],
   imports: [TypeOrmModule.forFeature([BarEntity])],
-  providers: [BarService],
+  providers: [BarService, BarRepository],
+  exports: [BarService, BarRepository]
 })
 export class BarModule { }
