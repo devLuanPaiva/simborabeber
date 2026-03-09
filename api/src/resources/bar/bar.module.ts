6b289bbf5caa558b0425c5372ac0942448204fa3
Luan Alves <devluanpaiva@gmail.com>
2026-03-09 11:31:22 -0300

    feat(api): importing entities

--- api/src/resources/bar/bar.module.ts
+++ api/src/resources/bar/bar.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BarService } from './bar.service';
 import { BarController } from './bar.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BarEntity } from './entities/bar.entity';
 
 @Module({
   controllers: [BarController],
+  imports: [TypeOrmModule.forFeature([BarEntity])],
   providers: [BarService],
 })
-export class BarModule {}
+export class BarModule { }
--- api/src/resources/user/user.module.ts
+++ api/src/resources/user/user.module.ts
@@ -4,11 +4,12 @@ import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
 import { UserRepository } from './repository/user.repository';
+import { BarEntity } from '../bar/entities/bar.entity';
 
 @Module({
   controllers: [UserController],
-  imports:[TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, BarEntity])],
   providers: [UserService, UserRepository],
-  exports:[UserService, UserRepository]
+  exports: [UserService, UserRepository]
 })
-export class UserModule {}
+export class UserModule { }
