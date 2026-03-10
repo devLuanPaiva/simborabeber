2c9fe7f4b44015164c1528eec23b947f827668aa
Luan Alves <devluanpaiva@gmail.com>
2026-03-10 10:43:41 -0300

    feat(api): implemenitng product dto and service

--- api/src/resources/product/product.service.ts
+++ api/src/resources/product/product.service.ts
@@ -1,26 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductRepository } from './repository/product.repository';
 
 @Injectable()
 export class ProductService {
+
+  constructor(private readonly productRepository: ProductRepository) { }
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    return this.productRepository.createProduct(createProductDto);
   }
 
   findAll() {
-    return `This action returns all product`;
+    return this.productRepository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string) {
+    return this.productRepository.findById(id);
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto) {
+    return this.productRepository.updateProduct(id, updateProductDto);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string) {
+    return this.productRepository.deleteProduct(id);
   }
 }
