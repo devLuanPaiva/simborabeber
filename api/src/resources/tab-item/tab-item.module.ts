1ab4f18e44b75892d26e15018e6c658feb6b9493
Luan Alves <devluanpaiva@gmail.com>
2026-03-12 13:25:00 -0300

    feat(api): exporting repository, controller, service and gateway

--- api/src/resources/tab-item/tab-item.module.ts
+++ api/src/resources/tab-item/tab-item.module.ts
@@ -1,8 +1,18 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TabItemService } from './tab-item.service';
 import { TabItemGateway } from './tab-item.gateway';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TabItemEntity } from './entities/tab-item.entity';
+import { AuthModule } from '../auth/auth.module';
+import { TabModule } from '../tab/tab.module';
+import { TabItemsRepository } from './repository/tab-item.repository';
+import { TabItemController } from './tab-item.controller';
+import { UserModule } from '../user/user.module';
 
 @Module({
-  providers: [TabItemGateway, TabItemService],
+  controllers: [TabItemController],
+  imports: [TypeOrmModule.forFeature([TabItemEntity]), AuthModule, forwardRef(() => TabModule), UserModule],
+  providers: [TabItemGateway, TabItemService, TabItemsRepository],
+  exports: [TabItemService, TabItemsRepository],
 })
-export class TabItemModule {}
+export class TabItemModule { }
