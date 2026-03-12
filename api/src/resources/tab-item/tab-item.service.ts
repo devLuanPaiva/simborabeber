6c6d349ab4e864e253cc366123670be89d0acfbb
Luan Alves <devluanpaiva@gmail.com>
2026-03-12 13:24:29 -0300

    feat(api): connecting service with repository

--- api/src/resources/tab-item/tab-item.service.ts
+++ api/src/resources/tab-item/tab-item.service.ts
@@ -1,25 +1,38 @@
 import { Injectable } from '@nestjs/common';
 import { CreateTabItemDto } from './dto/create-tab-item.dto';
 import { TabItemsRepository } from './repository/tab-item.repository';
+import { TabItemGateway } from './tab-item.gateway';
 
 @Injectable()
 export class TabItemService {
 
-  constructor(private readonly tabItemRepository: TabItemsRepository) { }
+  constructor(
+    private readonly tabItemRepository: TabItemsRepository,
+    private readonly tabItemGateway: TabItemGateway,
+  ) { }
 
-  createItemByTab(tabId: string, createTabItemDto: CreateTabItemDto, userId: string) {
-    return this.tabItemRepository.createItemByTab(tabId, createTabItemDto, userId);
+  async createItemByTab(tabId: string, createTabItemDto: CreateTabItemDto, userId: string) {
+    const created = await this.tabItemRepository.createItemByTab(tabId, createTabItemDto, userId);
+    await this.tabItemGateway.notifyItemAdded(tabId, created);
+    return created;
   }
 
   findItemsByTab(tabId: string) {
     return this.tabItemRepository.findItemsByTab(tabId);
   }
 
-  updateItemQuantity(id: string, quantity: number) {
-    return this.tabItemRepository.updateItemQuantity(id, quantity);
+  async updateItemQuantity(id: string, quantity: number) {
+    const updated = await this.tabItemRepository.updateItemQuantity(id, quantity);
+    const tabId = updated.tab?.id;
+    if (tabId) await this.tabItemGateway.notifyItemUpdated(tabId, updated);
+    return updated;
   }
 
-  deleteItem(id: string) {
-    return this.tabItemRepository.deleteItem(id);
+  async deleteItem(id: string) {
+    const item = await this.tabItemRepository.findByIdWithTab(id);
+    const tabId = item?.tab?.id;
+    await this.tabItemRepository.deleteItem(id);
+    if (tabId) await this.tabItemGateway.notifyItemDeleted(tabId, id);
+    return { message: 'Item removed' };
   }
 }
