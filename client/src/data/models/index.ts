1d92112f5cf45ed7db0f8e99d79a086685b196b5
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-03-15 17:00:33 -0300

    refactor(client): deleting unused interfaces

--- client/src/data/models/index.ts
+++ client/src/data/models/index.ts
@@ -1,7 +1,4 @@
-export * from './ISale';
+
 export * from './IUser';
-export * from './ITable';
 export * from './IProduct';
-export * from './IDelivery';
-export * from './IEstablishment';
 export * from './IBar';
