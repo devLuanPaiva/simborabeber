fe8c09d385d5dc1c30b606dbe7da1f44682fa0db
Luan Alves <devluanpaiva@gmail.com>
2026-03-19 11:27:52 -0300

    feat(client): creating panel bar products page

---
+++ client/src/app/painel/bar/[slug]/produtos/page.tsx
@@ -0,0 +1,17 @@
+import Menu from "@/components/bars/Menu";
+import { Header } from "@/components/layout/Header";
+import { panelBarProductsActions } from "./actions";
+
+export default async function PanelBarProductsPage(
+  props: Readonly<{ params: Promise<{ slug: string }> }>,
+) {
+  const { slug } = await props.params;
+  const products = await panelBarProductsActions(slug);
+
+  return (
+    <main className="min-h-screen ">
+      <Header slug_bar={slug} />
+      <Menu products={products} mode="panel" slug={slug} />
+    </main>
+  );
+}
