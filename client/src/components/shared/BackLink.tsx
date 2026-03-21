e2638723747ce86f3b271c89a190ebdaf61ecd3e
Luan Alves <devluanpaiva@gmail.com>
2026-03-21 09:32:29 -0300

    feat(client): creating BackLink shared component

---
+++ client/src/components/shared/BackLink.tsx
@@ -0,0 +1,18 @@
+import { ChevronLeft, Link } from "lucide-react";
+
+interface BackLinkProps {
+  slug: string;
+  label: string;
+}
+export function BackLink({ slug, label }: Readonly<BackLinkProps>) {
+  return (
+    <div className="mx-auto w-11/12 max-w-7xl flex justify-start py-6">
+      <Link
+        href={`/${slug}`}
+        className="flex items-center gap-1 bg-[#F2A20C] hover:bg-[#F28B0C] text-white cursor-pointer px-3 py-1 rounded-md text-sm font-medium"
+      >
+        <ChevronLeft /> {label}
+      </Link>
+    </div>
+  );
+}
