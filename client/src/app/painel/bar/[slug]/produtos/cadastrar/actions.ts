796975c1430bec16b1a73fc695a96fe45e5655bb
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-03-27 20:06:12 -0300

    feat(client): Assembling the complete URL and sending the CORS permission.

--- client/src/app/painel/bar/[slug]/produtos/cadastrar/actions.ts
+++ client/src/app/painel/bar/[slug]/produtos/cadastrar/actions.ts
@@ -14,9 +14,13 @@ export async function uploadImage(file: File) {
     const formData = new FormData();
     formData.append("file", file);
     formData.append("filename", file.name);
+    const base_url = process.env.NEXT_PUBLIC_API_URL || "";
 
-    const response = await fetch("/api/images/upload", {
+    const response = await fetch(`${base_url}/api/images/upload`, {
         method: "POST",
+        headers: {
+            "x-client-origin": base_url,
+        },
         body: formData,
     });
 
