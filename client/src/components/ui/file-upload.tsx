8303dae84e73bf67239d080317f5ecbf3e988b96
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-03-28 12:00:36 -0300

    style(client): improving component colors

--- client/src/components/ui/file-upload.tsx
+++ client/src/components/ui/file-upload.tsx
@@ -36,7 +36,7 @@ export const FileUpload = ({
 
   const handleFileChange = (newFiles: File[]) => {
     setFiles((prevFiles) => [...prevFiles, ...newFiles]);
-    onChange && onChange(newFiles);
+    onChange?.(newFiles);
   };
 
   const handleClick = () => {
@@ -53,7 +53,7 @@ export const FileUpload = ({
   });
 
   return (
-    <div className="w-full" {...getRootProps()}>
+    <div className="w-full border rounded" {...getRootProps()}>
       <motion.div
         onClick={handleClick}
         whileHover="animate"
@@ -70,11 +70,11 @@ export const FileUpload = ({
           <GridPattern />
         </div>
         <div className="flex flex-col items-center justify-center">
-          <p className="relative z-20 font-sans text-base font-bold text-neutral-700 dark:text-neutral-300">
-            Upload file
+          <p className="relative z-20 font-sans text-base font-bold text-zinc-700 dark:text-zinc-300">
+            Enviar Imagem
           </p>
-          <p className="relative z-20 mt-2 font-sans text-base font-normal text-neutral-400 dark:text-neutral-400">
-            Drag or drop your files here or click to upload
+          <p className="relative z-20 mt-2 font-sans text-base text-center font-normal text-zinc-400 dark:text-zinc-400">
+            Arraste e solte ou clique para selecionar um arquivo (PNG, JPG, JPEG)
           </p>
           <div className="relative mx-auto mt-10 w-full max-w-xl">
             {files.length > 0 &&
@@ -83,7 +83,7 @@ export const FileUpload = ({
                   key={"file" + idx}
                   layoutId={idx === 0 ? "file-upload" : "file-upload-" + idx}
                   className={cn(
-                    "relative z-40 mx-auto mt-4 flex w-full flex-col items-start justify-start overflow-hidden rounded-md bg-white p-4 md:h-24 dark:bg-neutral-900",
+                    "relative z-40 mx-auto mt-4 flex w-full flex-col items-start justify-start overflow-hidden rounded-md bg-white p-4 md:h-24 dark:bg-zinc-900",
                     "shadow-sm",
                   )}
                 >
@@ -92,7 +92,7 @@ export const FileUpload = ({
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       layout
-                      className="max-w-xs truncate text-base text-neutral-700 dark:text-neutral-300"
+                      className="max-w-xs truncate text-base text-zinc-700 dark:text-zinc-300"
                     >
                       {file.name}
                     </motion.p>
@@ -100,18 +100,18 @@ export const FileUpload = ({
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       layout
-                      className="shadow-input w-fit shrink-0 rounded-lg px-2 py-1 text-sm text-neutral-600 dark:bg-neutral-800 dark:text-white"
+                      className="shadow-input w-fit shrink-0 rounded-lg px-2 py-1 text-sm text-zinc-600 dark:bg-zinc-800 dark:text-white"
                     >
                       {(file.size / (1024 * 1024)).toFixed(2)} MB
                     </motion.p>
                   </div>
 
-                  <div className="mt-2 flex w-full flex-col items-start justify-between text-sm text-neutral-600 md:flex-row md:items-center dark:text-neutral-400">
+                  <div className="mt-2 flex w-full flex-col items-start justify-between text-sm text-zinc-600 md:flex-row md:items-center dark:text-zinc-400">
                     <motion.p
                       initial={{ opacity: 0 }}
                       animate={{ opacity: 1 }}
                       layout
-                      className="rounded-md bg-gray-100 px-1 py-0.5 dark:bg-neutral-800"
+                      className="rounded-md bg-zinc-100 px-1 py-0.5 dark:bg-zinc-800"
                     >
                       {file.type}
                     </motion.p>
@@ -121,7 +121,7 @@ export const FileUpload = ({
                       animate={{ opacity: 1 }}
                       layout
                     >
-                      modified{" "}
+                      Modificado{" "}
                       {new Date(file.lastModified).toLocaleDateString()}
                     </motion.p>
                   </div>
@@ -137,7 +137,7 @@ export const FileUpload = ({
                   damping: 20,
                 }}
                 className={cn(
-                  "relative z-40 mx-auto mt-4 flex h-32 w-full max-w-[8rem] items-center justify-center rounded-md bg-white group-hover/file:shadow-2xl dark:bg-neutral-900",
+                  "relative z-40 mx-auto mt-4 flex h-32 w-full max-w-[8rem] items-center justify-center rounded-md bg-white group-hover/file:shadow-2xl dark:bg-zinc-900",
                   "shadow-[0px_10px_50px_rgba(0,0,0,0.1)]",
                 )}
               >
@@ -145,13 +145,13 @@ export const FileUpload = ({
                   <motion.p
                     initial={{ opacity: 0 }}
                     animate={{ opacity: 1 }}
-                    className="flex flex-col items-center text-neutral-600"
+                    className="flex flex-col items-center text-zinc-600"
                   >
-                    Drop it
-                    <IconUpload className="h-4 w-4 text-neutral-600 dark:text-neutral-400" />
+                    Solte o arquivo aqui
+                    <IconUpload className="h-4 w-4 text-zinc-600 dark:text-zinc-400" />
                   </motion.p>
                 ) : (
-                  <IconUpload className="h-4 w-4 text-neutral-600 dark:text-neutral-300" />
+                  <IconUpload className="h-4 w-4 text-zinc-600 dark:text-zinc-300" />
                 )}
               </motion.div>
             )}
@@ -159,7 +159,7 @@ export const FileUpload = ({
             {!files.length && (
               <motion.div
                 variants={secondaryVariant}
-                className="absolute inset-0 z-30 mx-auto mt-4 flex h-32 w-full max-w-[8rem] items-center justify-center rounded-md border border-dashed border-sky-400 bg-transparent opacity-0"
+                className="absolute inset-0 z-30 mx-auto mt-4 flex h-32 w-full max-w-[8rem] items-center justify-center rounded-md border border-dashed border-[#F2A20C] bg-transparent opacity-0"
               ></motion.div>
             )}
           </div>
@@ -173,7 +173,7 @@ export function GridPattern() {
   const columns = 41;
   const rows = 11;
   return (
-    <div className="flex shrink-0 scale-105 flex-wrap items-center justify-center gap-x-px gap-y-px bg-gray-100 dark:bg-neutral-900">
+    <div className="flex shrink-0 scale-105 flex-wrap items-center justify-center gap-x-px gap-y-px bg-zinc-100 dark:bg-zinc-900">
       {Array.from({ length: rows }).map((_, row) =>
         Array.from({ length: columns }).map((_, col) => {
           const index = row * columns + col;
@@ -182,8 +182,8 @@ export function GridPattern() {
               key={`${col}-${row}`}
               className={`flex h-10 w-10 shrink-0 rounded-[2px] ${
                 index % 2 === 0
-                  ? "bg-gray-50 dark:bg-neutral-950"
-                  : "bg-gray-50 shadow-[0px_0px_1px_3px_rgba(255,255,255,1)_inset] dark:bg-neutral-950 dark:shadow-[0px_0px_1px_3px_rgba(0,0,0,1)_inset]"
+                  ? "bg-zinc-50 dark:bg-zinc-950"
+                  : "bg-zinc-50 shadow-[0px_0px_1px_3px_rgba(255,255,255,1)_inset] dark:bg-zinc-950 dark:shadow-[0px_0px_1px_3px_rgba(0,0,0,1)_inset]"
               }`}
             />
           );
