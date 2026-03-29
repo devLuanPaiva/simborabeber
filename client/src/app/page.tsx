7bec446443f909baa10d26057376d00d5f40e6fd
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-03-29 09:19:32 -0300

    refactor(client): cleaning home page

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,31 +1,9 @@
-import { BarsList } from "@/components/bars/BarsList";
-import { Beer } from "lucide-react";
-import { HomeActions } from "./actions";
+
 
 export default async function Home() {
-  const bars = await HomeActions();
-
   return (
     <main className="min-h-screen  p-8">
-      <div className="max-w-6xl mx-auto">
-        <header className="mb-10">
-          <h1 className="text-3xl font-bold text-[#F28B0C] flex items-center gap-2">
-            <Beer className="text-[#F2A20C]" />
-            Bares disponíveis
-          </h1>
-
-          <p className="text-zinc-600 mt-2">
-            Escolha um bar para acessar o cardápio
-          </p>
-        </header>
-        <BarsList bars={bars} />
-
-        {bars.length === 0 && (
-          <div className="text-center py-20 text-zinc-500">
-            Nenhum bar ativo encontrado
-          </div>
-        )}
-      </div>
+      simborabeber
     </main>
   );
 }
