490a2786c16cd7f7ba728afffde55f70b66ade15
Luan Alves <devluanpaiva@gmail.com>
2026-04-01 15:35:38 -0300

    refactor(client): deleting unused components

--- client/src/components/shared/Loading.tsx
+++
@@ -1,9 +0,0 @@
-import { LoaderOne } from "../ui/loader";
-
-export default function Loading() {
-    return (
-        <div className="mx-auto w-11/12 max-w-7xl h-screen flex flex-col items-center justify-center">
-            <LoaderOne />
-        </div>
-    )
-}
