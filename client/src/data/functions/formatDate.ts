0f540ff920d89437796572802d8fcee070de69a8
Luan Alves <97049835+Luan029@users.noreply.github.com>
2026-04-05 10:42:38 -0300

    feat(client): Set America/Sao_Paulo  timezone as the default in the formatDate function

--- client/src/data/functions/formatDate.ts
+++ client/src/data/functions/formatDate.ts
@@ -6,6 +6,7 @@ export function formatDate(dateString?: string | Date): string {
         month: '2-digit',
         year: 'numeric',
         hour: '2-digit',
-        minute: '2-digit'
+        minute: '2-digit',
+        timeZone: 'America/Sao_Paulo'
     });
 }
