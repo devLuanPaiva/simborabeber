5fbaeb9596275809369e064fbc989e499ea06b83
Luan Alves <devluanpaiva@gmail.com>
2026-04-08 08:22:01 -0300

    refactor(api):Importing the forward ref from the items module.

--- api/src/resources/reports/reports.module.ts
+++ api/src/resources/reports/reports.module.ts
@@ -4,10 +4,11 @@ import { ReportsController } from './reports.controller';
 import { TabModule } from '../tab/tab.module';
 import { BarModule } from '../bar/bar.module';
 import { AuthModule } from '../auth/auth.module';
+import { TabItemModule } from '../tab-item/tab-item.module';
 
 @Module({
   controllers: [ReportsController],
-  imports: [forwardRef(() => TabModule), forwardRef(() => BarModule), AuthModule],
+  imports: [forwardRef(() => TabModule), forwardRef(() => BarModule), forwardRef(() => TabItemModule), AuthModule],
   providers: [ReportsService],
 })
 export class ReportsModule { }
