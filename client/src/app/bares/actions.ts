b71f9bc4cecd93879f4018ea8acd2dbb1dc36e22
Luan Alves <devluanpaiva@gmail.com>
2026-04-21 16:00:59 -0300

    refactor(client): removing forced-cache in the request

--- client/src/app/bares/actions.ts
+++ client/src/app/bares/actions.ts
@@ -5,12 +5,7 @@ export async function getBares() {
     const base_url = process.env.NEXT_PUBLIC_BASE_URL;
 
     try {
-        const response = await fetch(`${base_url}/bar`, {
-            cache: "force-cache",
-            next: {
-                revalidate: 3600,
-            }
-        });
+        const response = await fetch(`${base_url}/bar`);
 
         if (!response.ok) {
             console.warn(`Erro ao buscar bares: ${response.status}`);
